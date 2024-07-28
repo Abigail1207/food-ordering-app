610abf25e0b79bf0ce1c5466f55744c27f5964af
Eunjeong Sim <tladmswjd113@naver.com>
2024-07-28 11:34:00 +0100

    second commit with updating design

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "lucide-react";
+import { Button } from "./ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "@radix-ui/react-separator";
+
+export default function MobileNav() {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span>Welcome to MernEats.com!</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+}
